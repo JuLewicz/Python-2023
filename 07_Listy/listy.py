f9f0293392f4cc302c40a042e47710e3c5460f9e
JuLewicz <justyna.lewicz@kramp.com>
2023-11-28 10:15:59 +0100

    sortowanie JL

--- 07_Listy/listy.py
+++ 07_Listy/listy.py
@@ -1,15 +1,15 @@
 l = [3, 5, 6, 7]
 l
 
-l[1]
+l[1]   #drugi element listy - w pythonie liczymy od 0!
 l[0]
 len(l)
-l[0:2]
+l[0:2]    #przedział otwarty z prawej
 l[1:2]
-l[1:]
+l[1:]    #lista od konkretnego elementu do końca
 l[:2]
-l[-1]
-l[1:-1]
+l[-1]     #ostatni element
+l[1:-1]    #OD DRUGIEGO DO przedostatniego elementu
 l[-4]
 l[1:-1] + l[0:-1]
 
@@ -42,3 +42,45 @@ s2 = '.|.'
 s2.join(["Ala", "ma", "kota"])
 
 '.' in s2
+
+
+napisy = []
+
+while True:
+    napis = input("Wprowadź dowolne słowo: ")
+
+    if not napis:
+        break
+
+    napisy.append(napis)
+    napisy.sort()
+
+print(napisy)
+
+
+
+
+
+napisy = []
+
+while True:
+    napis = input("Wprowadź dowolną liczbę: ")
+
+    if not napis:
+        break
+
+    try:
+        liczba = int(napis)
+    except ValueError:
+        print("To nie jest liczba. Spróbuj ponownie.")
+        continue
+
+    if liczba % 2 == 0:
+        ostatnia_parzysta = liczba
+
+try:
+    print("Ostatnia parzysta liczba:", ostatnia_parzysta)
+except NameError:
+    print("Nie wprowadzono żadnej parzystej liczby.")
+
+
