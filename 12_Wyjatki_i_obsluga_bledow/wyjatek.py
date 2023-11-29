059f4e11ecb17163dc6cea3b9bf8402fb96563da
JuLewicz <justyna.lewicz@kramp.com>
2023-11-29 09:49:27 +0100

    wyjątki JL

--- 12_Wyjatki_i_obsluga_bledow/wyjatek.py
+++ 12_Wyjatki_i_obsluga_bledow/wyjatek.py
@@ -5,7 +5,38 @@ try:
     print(f'Pod indeksem {i} jest element {l[i]}')
 except IndexError as e:
     print(e)
+except ValueError as e:
+    print("In value error")
+    print(e)
+except Exception as e:
+    print(e)
 else:
     print("Koniec")
 finally:
     print("Finally")
+
+
+while True:
+    try:
+        i = int(input("Podaj liczbę: "))
+        suma_cyfr = sum(int(a) for a in str(i))
+        print("Suma cyfr:", suma_cyfr)
+    except ValueError as e:
+        print("Wprowadzono nieprawidłową liczbę. Spróbuj ponownie.")
+        print(e)
+    except Exception as e:
+        print("Wystąpił nieoczekiwany błąd.")
+        print(e)
+    else:
+        print("Koniec")
+        break
+    finally:
+        print("Finally")
+
+
+
+
+
+
+
+
